360e24fad9872109da67ee8931b69e492701d310
Yuldoshova <ozoda8171@gmail.com>
2025-01-28 03:00:19 +0500

    added env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { appConfig, dbConfig } from '@config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true,
+    // load: [appConfig, dbConfig]
+  })],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { appConfig } from '@config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new ConfigService(appConfig)
+
+  await app.listen(
+    config.get<number>('appConfig.port'),
+    config.get<string>("appConfig.host"),
+    () => console.log(`Server ${config.get<number>('appConfig.port')}-port started...`)
+  );
 }
 bootstrap();
